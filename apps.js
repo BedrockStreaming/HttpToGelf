6877471b139c7c1456938d85539f55c20dff9914
Olivier Mansour <olivier.mansour@m6.fr>
2015-01-08 11:30:31 +0100

    initial commit

---
+++ apps.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var config = require('config').config;
+
+console.log(config);
+
+var security = require('./lib/security/security.js')(config.secretKey),
+    middleware = require('./lib/middleware.js');
+    express = require('express');
+
+var app = express();
+var gelfling = require('gelfling');
+
+var clientGelf = gelfling(config.host, config.port, {
+  defaults: {
+    	facility: config.facility,
+    	level: gelfling.INFO
+	}
+})
+
+var gelfRouting = require('./lib/routes/gelf.js')(clientGelf, config);
+
+app.post('/gelf/log/', middleware.securityToken(security), gelfRouting.gelfLog);
+
+
+app.get('/status', function (req, res){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200).end('OK');
+});
+
+
+var port = process.env.NODE_PORT || config.port;
+
+app.listen(port);
+console.log('Server running at http://127.0.0.1:'+port+' in '+app.settings.env+' mode');
